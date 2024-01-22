e34d1ed899bb3ee09e0e9250853595819531d3ef
XerShade <xershade.ca@gmail.com>
2024-01-22 07:13:19 -0500

    Added primary Camera2D node.

--- source/Nodes/GameEngine.cs
+++ source/Nodes/GameEngine.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 
 namespace Reoria.Nodes;
 
@@ -7,51 +7,57 @@ namespace Reoria.Nodes;
 /// </summary>
 public partial class GameEngine : Node2D
 {
-    /// <summary>
+	/// <summary>
 	/// The reference to the root <see cref="Node"/>.
 	/// </summary>
 	private static GameEngine? instance;
 
+	/// <summary>
+	/// The reference to the root <see cref="Node"/>.
+	/// </summary>
+	public static GameEngine? Instance
+	{
+		get
+		{
+			// Check to see if the root node is assigned, and return it if it has, if it has not then close the game.
+			return instance is not null ? instance
+				: throw new NullReferenceException("The root node reference has been lost, the game will now close.");
+		}
+	}
+
     /// <summary>
-    /// The reference to the root <see cref="Node"/>.
+    /// Defines the reference to the primary <see cref="Camera2D"/>.
     /// </summary>
-    public static GameEngine? Instance
-    {
-        get
-        {
-            // Check to see if the root node is assigned, and return it if it has, if it has not then close the game.
-            return instance is not null ? instance
-                : throw new NullReferenceException("The root node reference has been lost, the game will now close.");
-        }
-    }
+    [Export]
+    public Camera2D? Camera { get; protected set; }
 
     /// <summary>
     /// Constructs a new <see cref="GameEngine"/> instance and assignes the reference to it to <see cref="Instance"/>.
     /// </summary>
     public GameEngine()
-    {
-        // Check to see if the root node has already been created.
-        if (instance is not null)
-        {
-            // Yes it has, we can not allow "Dave" to do this.
-            throw new InvalidOperationException("The game can not create more then one root node, the game will now close.");
-        }
+	{
+		// Check to see if the root node has already been created.
+		if (instance is not null)
+		{
+			// Yes it has, we can not allow "Dave" to do this.
+			throw new InvalidOperationException("The game can not create more then one root node, the game will now close.");
+		}
 
-        // Assign this instance of the root node to the instance property.
-        instance = this;
-    }
+		// Assign this instance of the root node to the instance property.
+		instance = this;
+	}
 
-    /// <summary>
-    /// Disposes and releases resources used by the class.
-    /// </summary>
-    ~GameEngine()
-    {
-        // Check to see if the root node has already been created.
-        if (instance is not null)
-        {
-            // Assign this instance of the root node to the instance property.
-            instance = null;
-        }
+	/// <summary>
+	/// Disposes and releases resources used by the class.
+	/// </summary>
+	~GameEngine()
+	{
+		// Check to see if the root node has already been created.
+		if (instance is not null)
+		{
+			// Assign this instance of the root node to the instance property.
+			instance = null;
+		}
     }
 
     /// <summary>
@@ -60,5 +66,12 @@ public partial class GameEngine : Node2D
     /// callbacks get triggered first, and the parent node will receive the ready 
     /// notification afterwards.
     /// </summary>
-    public override void _Ready() => base._Ready();
+    public override void _Ready()
+    {
+        // Fetch and assign the child nodes if they have not been assigned yet.
+        this.Camera ??= this.GetNode<Camera2D>("Camera");
+
+        // Pass to the base class' function.
+        base._Ready();
+    }
 }
