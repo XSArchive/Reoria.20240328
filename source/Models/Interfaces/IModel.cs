33397ce0b6d20f856cf91ca0d50bbdd06c89e042
XerShade <xershade.ca@gmail.com>
2024-02-03 21:48:34 -0500

    Added Model class.

---
+++ source/Models/Interfaces/IModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Reoria.Models.Interfaces;
+
+/// <summary>
+/// Provides the properties and functions of data models.
+/// </summary>
+public interface IModel
+{
+
+}
---
+++ source/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿using Godot;
+using Reoria.Models.Interfaces;
+
+namespace Reoria.Models;
+
+/// <summary>
+/// Provides the properties and functions of data models.
+/// </summary>
+[GlobalClass]
+public abstract partial class Model : Resource, IModel
+{
+
+}
